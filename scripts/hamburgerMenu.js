f2ecb0e1eeebd5ffd2d09b270c52ee2af1fad5f4
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-07-04 23:59:55 +0200

    Responsiveness
    Improved mobile responsiveness, added hamburger menu for mobile and changed some layout designs

---
+++ scripts/hamburgerMenu.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("nav-links");
+    if (x.classList.contains("responsive")) {
+        x.classList.remove("responsive");
+    } else {
+        x.classList.add("responsive");
+    }
+}
+
+function closeMenu() {
+  var x = document.getElementById("nav-links");
+  x.classList.remove("responsive");
+}
